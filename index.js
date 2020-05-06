0fbae7605933b14dd55afd77647e40c88cb3ed27
HyunIn Sim <>
2020-05-06 12:45:22 +0900

    Move Private Information 20200506

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:admin@mongodb-m2ipz.mongodb.net/test?retryWrites=true&w=majority",
-    { useUnifiedTopology: true, useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("MongoDB Error!!", err));
 
-app.get("/", function (req, res) {
-  res.send("hello world");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ Wtf: "WTFFFF" });
+});
+
+app.post("/api/user/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(5000);
+console.log("Server is Running at Port 5000!");
